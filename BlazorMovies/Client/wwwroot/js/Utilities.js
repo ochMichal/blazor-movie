73aea476525958ce705e1624f4a0d06a71f1e93a
Michal Och <michal.och@pl.abb.com>
2020-07-07 22:31:05 +0200

    first commit

---
+++ BlazorMovies/Client/wwwroot/js/Utilities.js
@@ -0,0 +1,14 @@
+﻿function myfunction(message) {
+    console.log("from utilities " + message);
+}
+
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("BlazorMovies.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript " + result);
+        });
+}
+
+function dotnetInstanceInvocation(dotnetHelper) {
+    dotnetHelper.invokeMethodAsync("IncrementCount");
+}
