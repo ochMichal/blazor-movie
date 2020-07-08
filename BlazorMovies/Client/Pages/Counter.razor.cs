1fc41599d6574839506c465ba66ad53480cdb28c
Michal Och <michal.och@pl.abb.com>
2020-07-08 21:46:57 +0200

    Routing done

--- BlazorMovies/Client/Pages/Counter.razor.cs
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -11,45 +11,12 @@ namespace BlazorMovies.Client.Pages
 {
     public partial class Counter
     {
-        [Inject] SingletonService singleton { get; set; }
-        [Inject] TransientService transient { get; set; }
-        [Inject] IJSRuntime js { get; set; }
-        [CascadingParameter] public AppState AppState { get; set; }
-
-
-        private List<Movie> movies;
-        protected override void OnInitialized()
-        {
-            movies = new List<Movie>()
-            {
-                new Movie(){Title="Joker", ReleaseDate = new DateTime(2019,7,2)},
-                new Movie(){Title="Avengers", ReleaseDate = new DateTime(2016,11,23)},
-            };
-        }
-
         private int currentCount = 0;
-        private static int currentCountStatic = 0;
 
-        [JSInvokable]
-        public async Task IncrementCount()
+        public void IncrementCount()
         {
             currentCount++;
-            transient.Value = currentCount;
-            singleton.Value = currentCount;
-            currentCountStatic++;
-            await js.InvokeVoidAsync("dotnetStaticInvocation");
         }
 
-        private async Task IncrementCountJavaScript()
-        {
-            await js.InvokeVoidAsync("dotnetInstanceInvocation",
-                DotNetObjectReference.Create(this));
-        }
-
-        [JSInvokable]
-        public static Task<int> GetCurrentCount()
-        {
-            return Task.FromResult(currentCountStatic);
-        }
     }
 }
